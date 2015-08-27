b0f40925d27844f1594ec5bdd17a1ff2aa7d5488
theosbornidentity <osborna01@gmail.com>
2015-08-27 14:41:40 -0400

    added loading spinner and animations for history logs

--- client/templates/dashboard/history.js
+++ client/templates/dashboard/history.js
@@ -1,3 +1,14 @@
+Template.history.onRendered(function () {
+  this.find('.wrapper')._uihooks = {
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    }
+  }
+});
+
 Template.log.helpers({
   getStyle: function() {
     switch(this.action) {
