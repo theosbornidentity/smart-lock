8ebdc7ac3ba267caf1fbed5e22e8c8542d964862
theosbornidentity <osborna01@gmail.com>
2015-10-18 18:38:34 -0400

    added user filtering

--- client/templates/dashboard/history.js
+++ client/templates/dashboard/history.js
@@ -1,61 +1,6 @@
-/*
-Note: tried to make an array in history.js to keep track of all filtered logs depending on the query.
-*/
-
-var filteredlogs = new Array();
-
-/*
-Note: tried to make populate the filtered logs depending on the query in the dropdown menu. Would match the 
-name in the dropdown menu (first + last name) with name from the log <- 
-I assumed the name would be firstName + " " lastName in the log. Essentially matching name Strings between all the logs
-and the dropdown menu.
-*/
-
-function populateFilteredLogs() {
-  var e = document.getElementById("userdropdown");
-  var nameOfDropDown = e.options[e.selectedIndex].text;
-
-
-  for(i = 0 ; i < logs.length ; i++) {
-    console.log(nameOfDropDown + " compared to " + logs[i].user);
-    var nameOfLog = logs[i].user;
-    if(nameOfLog == nameOfDropDown) {
-      filteredlogs.push(logs[i]);
-    }
-  }
-  
-}
-
-/*
-Note: Referenced the Template.log.helpers for this one. I used this primarily to populate the dropdown menu with first/last names needed. 
-*/
-
-Template.uInfo.helpers({
-  getInfo: function () {
-    return this.profile.firstName + " " + this.profile.lastName;
-  }
+Template.history.onCreated(function () {
+  this.userFilter = new ReactiveVar('');
 });
-
-
-/*
-could not seem to use Template.log.helpers to populate the table in the "User History" tab so I made a new tab. 
-*/
-Template.filteredlog.helpers({
-  getStyle: function() {
-    switch(this.action) {
-      case 'Unlocked':
-          return 'success';
-      case 'Locked':
-          return 'danger';
-      default:
-          return 'default';
-    };
-  },
-  getTime: function() {
-    return moment(this.date).calendar();
-  }
-});
-
 
 Template.history.onRendered(function () {
   this.find('.wrapper')._uihooks = {
@@ -65,6 +10,32 @@ Template.history.onRendered(function () {
         .insertBefore(next)
         .fadeIn();
     }
+  }
+});
+
+Template.history.helpers({
+  logs: function() {
+    var userName = Template.instance().userFilter.get();
+    if(userName === '') return Logs.find({}, {sort: {date: -1}});
+    return Logs.find({user: userName}, {sort: {date: -1}});
+  },
+  currentUserFilter: function () {
+    var userName = Template.instance().userFilter.get();
+    if(userName === '') return "Users";
+    else return 'User: ' + userName;
+  }
+});
+
+Template.history.events({
+	'click .dropdown-toggle': function(event) {
+		event.stopPropagation();
+    $('#user-dropdown').toggleClass('open');
+	},
+  'click #user-filter': function(event, template) {
+    template.userFilter.set(this.profile.firstName + ' ' + this.profile.lastName);
+  },
+  'click #no-user-filter': function(event, template) {
+    template.userFilter.set('');
   }
 });
 
@@ -83,5 +54,3 @@ Template.log.helpers({
     return moment(this.date).calendar();
   }
 });
-
-
